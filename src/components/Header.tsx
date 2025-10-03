c8b1ec318f7aaca0c145b35ab0d7f4ef2fc6d2bb
Felipe Campos Macedo <felipecamposmacedo@gmail.com>
2025-10-03 02:12:19 -0300

    fix: add logo on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,10 @@ const Header = () => {
 
   return (
     <div
-      className="bg-slate-900 py-6 px-6 text-white cursor-pointer"
+      className="bg-slate-900 py-6 px-6 text-white cursor-pointer flex gap-2 items-center"
       onClick={reloadPage}
     >
+        <img src="logo.png" className="max-w-8 max-h-8" alt="logo" />
       <h1 className="text-xl font-bold">GitHub User Finder</h1>
     </div>
   );
